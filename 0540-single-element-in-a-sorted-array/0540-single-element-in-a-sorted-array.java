51df9f39361c777cdd338273618e40c40f5aff89
Siddhanth Roy <roysiddhanth007@gmail.com>
2026-05-29 12:59:46 +0530

    Time: 0 ms (100%), Space: 53 MB (36.89%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,12 +1,43 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        HashMap <Integer,Integer> map = new HashMap<>();
-       for(int i=0; i<nums.length; i++){
-            map.put(nums[i], map.getOrDefault(nums[i],0)+1);
+       int n=nums.length;
+       int start=0;
+       int end=n-1;
+       //single element
+       if(n == 1){
+        return nums[0];
        }
-       for(Integer Key : map.keySet()){
-        if(map.get(Key)==1){
-            return Key;
+
+       while(start<=end){
+        int mid = start + (end - start)/2;
+        
+        //Corner elements
+        if(mid==0 && nums[mid]!=nums[mid+1]){
+            return nums[mid];
+        }
+        if(mid==n-1 && nums[mid]!=nums[mid-1]){
+            return nums[mid];
+        }
+
+        if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
+            return nums[mid];
+        }
+        //Even elements on left and right sides
+        if(mid%2==0){
+            if(nums[mid]==nums[mid-1]){
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        else{ //Odd elements on left and right sides
+            if(nums[mid]==nums[mid-1]){
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
         }
        }
        return -1;
