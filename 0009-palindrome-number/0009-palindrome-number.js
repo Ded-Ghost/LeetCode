5d3376bd468aeada972ae635544cc316fd771425
Siddhanth Roy <roysiddhanth007@gmail.com>
2026-06-08 23:53:41 +0530

    Time: 3 ms (98.22%), Space: 63.6 MB (66.95%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if(x<0){
+        return false;
+    }
+    let n = x;
+    let rev=0;
+    while(n!=0)
+    {
+        rev = rev*10 + (n%10);
+        n = Math.floor(n/10);
+    }
+    
+    return rev === x;
+};
