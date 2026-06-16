a47beacdcd34f206eee3502228d6bef27e41fef2
Siddhanth Roy <roysiddhanth007@gmail.com>
2026-06-16 21:01:56 +0530

    Time: 4 ms (76.47%), Space: 55.2 MB (58.82%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder res = new StringBuilder();
+
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+
+            if(ch=='*'){   
+                if(res.length()>0){//second case for first character
+                   res.deleteCharAt(res.length()-1);
+                }
+            }
+            else if(ch=='#'){
+                res.append(res);
+            }
+            else if(ch=='%'){
+                res.reverse();
+            }
+            else{
+                res.append(ch);
+            }
+        }
+
+        String reverse = res.toString();
+
+        return reverse;
+    }
+}
